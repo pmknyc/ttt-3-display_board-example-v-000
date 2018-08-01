e9799dde4d087379a4f80407e9ea0db960752d7f
Paula McKinley <pmknyc@optimum.net>
2018-08-01 00:07:19 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+puts "A Tic Tac Toe board"
+def display_board
+  board = Array.new(9, "   ")
+  parts = ["", "|","-----------"]
+  puts "#{board[0]}#{parts[1]}#{board[1]}#{parts[1]}#{board[2]}"
+  puts "#{parts[2]}"
+  puts "#{board[0]}#{parts[1]}#{board[1]}#{parts[1]}#{board[2]}"
+  puts "#{parts[2]}"  
+  puts "#{board[0]}#{parts[1]}#{board[1]}#{parts[1]}#{board[2]}"
+end
+display_board
