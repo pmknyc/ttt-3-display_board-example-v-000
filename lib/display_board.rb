0916424602fc0bfeebef03f3363d48d532956c84
Paula McKinley <pmknyc@optimum.net>
2018-08-12 02:21:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@
 puts "A Tic Tac Toe board"
 def display_board
   board = Array.new(9, "   ")
-  parts = ["", "|","-----------"]
-  puts "#{board[0]}#{parts[1]}#{board[1]}#{parts[1]}#{board[2]}"
-  puts "#{parts[2]}"
-  puts "#{board[0]}#{parts[1]}#{board[1]}#{parts[1]}#{board[2]}"
-  puts "#{parts[2]}"  
-  puts "#{board[0]}#{parts[1]}#{board[1]}#{parts[1]}#{board[2]}"
+  parts = ["|","-----------"]
+  puts "#{board[0]}#{parts[0]}#{board[1]}#{parts[0]}#{board[2]}"
+  puts "#{parts[1]}"
+  puts "#{board[3]}#{parts[0]}#{board[4]}#{parts[0]}#{board[5]}"
+  puts "#{parts[1]}"
+  puts "#{board[6]}#{parts[0]}#{board[7]}#{parts[0]}#{board[8]}"
 end
 display_board
--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -8,31 +8,26 @@ describe '/lib/display_board.rb' do
   context "#display_board method" do
     it 'represents a cell as a string with 3 spaces' do
       output = capture_puts{ display_board }
-
       expect(output).to include("   ")
     end
-    
+
     it 'separates cells with a | character' do
       output = capture_puts{ display_board }
-
-      expect(output).to include("   |   ")      
+      expect(output).to include("   |   ")
     end
 
     it 'prints an 3 cell row' do
       output = capture_puts{ display_board }
-
       expect(output).to include("   |   |  ")
     end
 
     it 'separates rows with a line of 11 -' do
       output = capture_puts{ display_board }
-
       expect(output).to include("-----------")
     end
 
     it 'prints a 3x3 tic tac toe board' do
       output = capture_puts{ display_board }
-
       expected_output  = "   |   |   \n"
       expected_output += "-----------\n"
       expected_output += "   |   |   \n"
